9f7f800b6173457c95ee2fb1c6dc51c701b152a4
Kazuhiko TAMURA <tamura@tecnet.co.jp>
2015-05-20 18:27:55 +0900

    initial version

---
+++ src/Module/DaoBuilderBearModule.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Omelet\Module;
+
+use Ray\Di\AbstractModule;
+use Ray\Di\Scope;
+use Ray\DbalModule\DbalModule;
+
+use Omelet\Builder\DaoBuilderContext;
+
+class DaoBuilderBearModule extends AbstractModule {
+    /**
+     * @var array
+     */
+    private $config;
+    /**
+     * @var array
+     */
+    private $interfaces;
+    
+    public function __construct(array $config, array $interfaces)
+    {
+        $this->config = $config;
+        $this->interfaces = $interfaces;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $context = new DaoBuilderContext($this->config);
+        $this->bind(DaoBuilderContext::class)->toInstance($context);
+        
+        $this->install(new DbalModule($context->connectionString()));
+        
+        foreach ($this->interfaces as $intf) {
+            $context->build($intf);
+
+            $this->bind($intf)->to($context->getDaoClassName($intf))->in(Scope::SINGLETON);
+        }
+    }
+}
